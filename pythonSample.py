09d918fa803044d5051cbb2b5a817739d000bc47
longlili <18770814559@163.com>
2025-05-06 22:42:43 +0800

    add features

--- pythonSample.py
+++ pythonSample.py
@@ -1,6 +1,6 @@
 print("Hello, World!")
-print("This is a sample Python script.")
-print("It demonstrates how to print text to the console.")
+print("This is a sample Python script.!!!")
+print("It demonstrates how to print text to the console.+++")
 print("You can run this script using Python.")
 print("Make sure you have Python installed on your system.")
 print("To run the script, use the command: python pythonSample.py")
@@ -9,3 +9,7 @@ print("This script is a simple demonstration of Python syntax.")
 print("You can modify this script to suit your needs.")
 print("Happy coding!")
 print("End of the script.")
+a = 5
+b = 10
+c = a + b
+print("The sum of", a, "and", b, "is:", c)
